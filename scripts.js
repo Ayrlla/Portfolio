ea03e70e99d3111319ac7686f52d1b46bf9cafe5
Ayrlla <ayrllagabriela13@gmail.com>
2024-05-23 12:36:05 -0300

    Adicionando as alterações solicitadas

--- scripts.js
+++ scripts.js
@@ -1,25 +1,50 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const nav = document.querySelector('.nav-links');
-    const burger = document.querySelector('.burger');
-    const scrollToTopBtn = document.getElementById('scrollToTop');
-    
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-        burger.classList.toggle('toggle');
-    });
-
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 300) {
-            scrollToTopBtn.style.display = 'block';
-        } else {
-            scrollToTopBtn.style.display = 'none';
-        }
-    });
-
-    scrollToTopBtn.addEventListener('click', () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-    });
+const ani = ScrollReveal({
+	distance: '35px',
+	duration: 2200,
+	reset: true
 });
+ani.reveal('.side-1', {
+	delay: 500,
+	origin: 'top'
+});
+ani.reveal('.side-2', {
+	delay: 600,
+	origin: 'bottom'
+});
+ani.reveal('.about-img', {
+	delay: 500,
+	origin: 'top'
+});
+ani.reveal('.about-text', {
+	delay: 600,
+	origin: 'bottom'
+});
+ani.reveal('.section-header', {
+	delay: 500,
+	origin: 'top'
+});
+ani.reveal('.services-content', {
+	delay: 700,
+	origin: 'bottom'
+});
+ani.reveal('.contact-content', {
+	delay: 700,
+	origin: 'bottom'
+});
+window.onscroll = function() {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		scrollToTopBtn.style.display = "block";
+	} else {
+		scrollToTopBtn.style.display = "none";
+	}
+}
+
+function scrollToTop() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
